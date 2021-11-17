f85b392dc4462af64da00449b4555df0045949f0
ImmanuelM404 <immanuelmamou@gamil.com>
2021-11-17 14:10:17 -0800

    new updates

--- server/index.js
+++ server/index.js
@@ -87,3 +87,9 @@ app.use(express.static(path.join(__dirname, 'client/frontend.js')))
  app.get('/', (req,res)=>{
  res.sendFile(path.join(__dirname, '../index.html'))
  })
+
+ const port = process.env.PORT || 4000
+
+app.listen(port, ()=>{
+    console.log(`App is running on ${port}`);
+})
